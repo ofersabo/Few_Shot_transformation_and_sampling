dc927a6da57d100ec2acf0558963deed588cff7d
ofersabo <ofersabo@gmail.com>
2020-03-21 11:34:50 +0200

    first commit

--- TACRED_create_list_per_relation.py
+++ TACRED_create_list_per_relation.py
@@ -1,12 +1,8 @@
 import os
 import json
 import random
-data_location = "raw/"
-# with open(data_location,"r") as f:
-#     doc = json.load(f)
-#     for d in doc:
-        # print(d)
 
+data_location = "raw/"
 
 def get_set_of_relation_type(file):
     global type_relation
@@ -18,7 +14,7 @@ def get_set_of_relation_type(file):
 
 
 def replace_to_rounded_parnthesis(list_of_tokens):
-    for i,token in enumerate(list_of_tokens):
+    for i, token in enumerate(list_of_tokens):
         if token == '-LRB-':
             list_of_tokens[i] = '('
         elif token == '-RRB-':
@@ -26,19 +22,18 @@ def replace_to_rounded_parnthesis(list_of_tokens):
     return list_of_tokens
 
 
-
 def read_json(data_location):
     directory = os.fsencode(data_location)
     all_data_dict_per_relation_list = {}
-    train = {}
-    dev = {}
-    test = {}
+    data_sections = {"train":{},"dev":{},"test":{}}
     for file in os.listdir(directory):
         dataset = file.decode("utf-8")
         dataset = dataset.split('.')[0]
-        this_dataset_by_relation = eval(dataset)
+        if dataset not in data_sections: continue
+        print(dataset)
+        this_dataset_by_relation = data_sections[dataset]
         filename = os.path.join(directory, file)
-        with open(filename,"r") as fp:
+        with open(filename, "r") as fp:
             data = json.load(fp)
         for x in data:
             tokens = replace_to_rounded_parnthesis(x['token'])
@@ -46,14 +41,14 @@ def read_json(data_location):
             head_end = x['subj_end']
             tail_start = x['obj_start']
             tail_end = x['obj_end']
-            head = " ".join(tokens[head_start:head_end+1]).lower()
-            tail = " ".join(tokens[tail_start:tail_end+1]).lower()
+            head = " ".join(tokens[head_start:head_end + 1]).lower()
+            tail = " ".join(tokens[tail_start:tail_end + 1]).lower()
             relation = x['relation']
-            this_small_dict = {"tokens": tokens,"h":[head,None,[[i for i in range(head_start,head_end+1)]]]
-                ,"t":[tail,None,[[i for i in range(tail_start,tail_end+1)]]]}
-            insert_to_dict_data(all_data_dict_per_relation_list, relation, this_small_dict)
-            insert_to_dict_data(this_dataset_by_relation, relation, this_small_dict)
-        with open("raw/TACRED_"+dataset+".json","w") as f:
+            instance_dict = {"tokens": tokens, "h": [head, None, [[i for i in range(head_start, head_end + 1)]]]
+                , "t": [tail, None, [[i for i in range(tail_start, tail_end + 1)]]]}
+            insert_to_dict_data(all_data_dict_per_relation_list, relation, instance_dict)
+            insert_to_dict_data(this_dataset_by_relation, relation, instance_dict)
+        with open("raw/TACRED_" + dataset + ".json", "w") as f:
             json.dump(this_dataset_by_relation, f)
 
     # with open("TACRED_fewshot_merged.json", "w") as f:
@@ -67,9 +62,4 @@ def insert_to_dict_data(dict_vat, relation, this_small_instance):
 
 
 all_data = read_json(data_location)
-
-no_rel = all_data["no_relation"]
-filtered_no_rel = random.sample(no_rel,15)
-from pprint import pprint
-pprint(filtered_no_rel)
 
