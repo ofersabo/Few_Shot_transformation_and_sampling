aff118245b671616ccdfb4a4c31bfdaec84b2713
Ofer <>
2020-09-08 09:31:04 +0300

    convert TACRED to list by relation

--- convert_dataset_to_list_by_relation.py
+++ convert_dataset_to_list_by_relation.py
@@ -1,13 +1,36 @@
 import json
 import os, argparse
 
+def add_additional_data(x):
+    tokens = replace_to_rounded_parnthesis(x['token'])
+    head_start = x['subj_start']
+    head_end = x['subj_end']
+    tail_start = x['obj_start']
+    tail_end = x['obj_end']
+    head = " ".join(tokens[head_start:head_end + 1]).lower()
+    tail = " ".join(tokens[tail_start:tail_end + 1]).lower()
+    relation = x['relation']
+    instance_dict = {"tokens": tokens, "h": [head, None, [[i for i in range(head_start, head_end + 1)]]]
+        , "t": [tail, None, [[i for i in range(tail_start, tail_end + 1)]]]}
+    instance_info = {**x, **instance_dict}
+    return instance_info,relation
+
+
+def replace_to_rounded_parnthesis(list_of_tokens):
+    for i, token in enumerate(list_of_tokens):
+        if token == '-LRB-':
+            list_of_tokens[i] = '('
+        elif token == '-RRB-':
+            list_of_tokens[i] = ')'
+    return list_of_tokens
+
 def read_json(data_location,output_file):
     this_dataset_by_relation = {}
     with open(data_location, "r") as fp:
         data = json.load(fp)
     for x in data:
-        relation = x['relation']
-        insert_to_dict_data(this_dataset_by_relation, relation, x)
+        instance_dict, relation = add_additional_data(x)
+        insert_to_dict_data(this_dataset_by_relation, relation, instance_dict)
     with open(output_file, "w") as f:
         json.dump(this_dataset_by_relation, f)
 
