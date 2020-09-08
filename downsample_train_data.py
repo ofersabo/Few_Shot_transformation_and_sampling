0f6ac1bad6805319ed3fc7074e4721316e3396ed
Ofer <>
2020-09-08 08:45:59 +0300

    script to downsample the NOTA category in the training data

--- downsample_train_data.py
+++ downsample_train_data.py
@@ -1,21 +1,33 @@
 import json,sys
+import argparse
 
-def do_downsampling_and_output_file(numbers):
+def do_downsampling_and_output_file(numbers,data_original,output):
     no_rel_original = data_original["no_relation"]
     new_no_rel = [no_rel_original[i] for i in numbers]
     final_data = data_original
     final_data["no_relation"] = new_no_rel
-    with open("new_downsampled_train_data.json","w") as f:
+    with open(output,"w") as f:
         json.dump(final_data,f)
 
 
-original_data = sys.argv[1]
-original_data = open(original_data)
-data_original = json.load(original_data)
+def main(args):
+    original_data = args.dataset
+    original_data = open(original_data)
+    data_original = json.load(original_data)
+    with open("numbers_for_downsampling.txt","r") as f:
+        x = json.load(f)
 
-with open("numbers_for_downsampling.txt","r") as f:
-    x = json.load(f)
+    numbers = x["the_required_numbers"]
 
-numbers = x["the_required_numbers"]
+    do_downsampling_and_output_file(numbers,data_original,args.output_file)
 
-do_downsampling_and_output_file(numbers)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default=None, type=str, required=True,
+                        help="Pointer to train dataset, to be downsampled")
+
+    parser.add_argument("--output_file", type=str, required=False,
+                        help="The output file where the downsampled dataset is stored.")
+
+    _args = parser.parse_args()
+    main(_args)
